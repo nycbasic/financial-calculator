cc718d751d0eb02b13d3bf6ce12f26991c987ace
Sam Poon <samuel.poon@baruchmail.cuny.ed>
2018-06-26 10:15:40 -0400

    revised to OOP

--- js/app.js
+++ js/app.js
@@ -4,8 +4,27 @@ const salaryInput      = document.querySelector('.form-control'),
 
 let salary = 0;
 
+const expenses = {
+    housing: 0.35,
+    utilities: 0.05,
+    food: 0.15,
+    transportation: 0.1,
+    medical: 0.03,
+    clothing: 0.05,
+    personal: 0.07,
+    savings: 0.1,
+    debt: 0.1,
+    total: function(id) {
+        let selector     = document.getElementById(id),
+            expenseTotal = salary * this[id];
+    
+        selector.innerText = "$" + expenseTotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+    }
+}
+
 salaryInput.addEventListener('keydown', (e) => {
-    if(e.keyCode === 13) {
+    let keyEvent = e.keyCode === 13;
+    if(keyEvent) {
         e.preventDefault();
         salary = Number(salaryInput.value);
         salaryInput.value = "";
@@ -13,50 +32,10 @@ salaryInput.addEventListener('keydown', (e) => {
         setTimeout(() => monthly.innerText = "$" + salary.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
 
         for(let val of expenseSelectors) {
-            expenses(val.id);
+            expenses.total(val.id);
          }
     } 
 }) 
-
-function expenses(id) {
-    let percent        = expensePercent(id),
-        selector       = document.getElementById(id),
-        expenseTotal = salary * percent;
-
-    selector.innerText = "$" + expenseTotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-}
-
-function expensePercent(id) {
-    const expense = {
-        housing: 0.35,
-        utilities: 0.05,
-        food: 0.15,
-        transportation: 0.1,
-        medical: 0.03,
-        clothing: 0.05,
-        personal: 0.07,
-        savings: 0.1,
-        debt: 0.1
-    }
-    return expense[id];
-}
-
-// const testObj = {
-//     housing: 0.35,
-//     utilities: 0.05,
-//     food: 0.15,
-//     transportation: 0.1,
-//     medical: 0.03,
-//     clothing: 0.05,
-//     personal: 0.07,
-//     savings: 0.1,
-//     debt: 0.1,
-//     expenseTotal: function(id) {
-//        return salary*this[id];
-//     }
-// }
-
-
 
 
 
