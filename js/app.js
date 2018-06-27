a265dee039b6add65e49f7333c489f0b6aa36575
Sam Poon <spoon@Sams-MBP.fios-router.home>
2018-06-26 23:05:59 -0400

    revision to OOP

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@ const salaryInput      = document.querySelector('.form-control'),
       monthly          = document.getElementById('monthly'),
       expenseSelectors = document.querySelectorAll('.expense');
 
-let salary = 0;
-
 const expenses = {
+    salary: 0,
     housing: 0.35,
     utilities: 0.05,
     food: 0.15,
@@ -13,29 +12,38 @@ const expenses = {
     clothing: 0.05,
     personal: 0.07,
     savings: 0.1,
-    debt: 0.1,
+    debt: 0.1, 
     total: function(id) {
         let selector     = document.getElementById(id),
-            expenseTotal = salary * this[id];
-    
+            expenseTotal = this.salary * this[id];
+
         selector.innerText = "$" + expenseTotal.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
     }
 }
 
 salaryInput.addEventListener('keydown', (e) => {
-    let keyEvent = e.keyCode === 13;
-    if(keyEvent) {
-        e.preventDefault();
-        salary = Number(salaryInput.value);
-        salaryInput.value = "";
-        
-        setTimeout(() => monthly.innerText = "$" + salary.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+    const keyEvent = e.keyCode === 13,
+          numbers  = /^[0-9]+$/;
 
-        for(let val of expenseSelectors) {
-            expenses.total(val.id);
-         }
+    if(keyEvent) {
+        if(salaryInput.value.match(numbers)) {
+            e.preventDefault();
+            expenses.salary = Number(salaryInput.value);
+            salaryInput.value = "";
+
+            setTimeout(() => monthly.innerText = "$" + expenses.salary.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+
+            for(let val of expenseSelectors) {
+                expenses.total(val.id);
+            }
+        } else {
+            e.preventDefault();
+            salaryInput.value = "";
+            salaryInput.setAttribute('placeholder', 'Please Enter A Valid Salary!');
+        }  
     } 
-}) 
+})
+
 
 
 
